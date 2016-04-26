05681ef56e557999ae215f8d17b3efa1af7b2c93
Elvis Chiang <elvisfb@gmail.com>
2016-04-26 18:05:42 +0800

    fix url

--- js/def.js
+++ js/def.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  SERVER_DATA_YEAR: "http://csie0.cs.ccu.edu.tw:8080/year",
-  SERVER_DATA_TODAY: "http://csie0.cs.ccu.edu.tw:8080/today",
+  SERVER_DATA_YEAR: "http://localhost:8080/year",
+  SERVER_DATA_TODAY: "http://localhost:8080/today",
 };
