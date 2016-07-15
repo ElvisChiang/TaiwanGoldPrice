3c79cc21dd33af9a8d2b6aa45b354c5861ef8a62
Elvis Chiang <elvisfb@gmail.com>
2016-07-15 19:24:30 +0800

    upgrade to react-native 0.29.0
    - drop crashlytics support for now
    - upgrade other modules to latest
    - use xUnique to lint xcode project file
    - open http insecure connection for now

--- android/app/src/main/java/com/gmail.elvisfb/goldprice/MainActivity.java
+++ android/app/src/main/java/com/gmail.elvisfb/goldprice/MainActivity.java
@@ -1,26 +1,8 @@
 package com.gmail.elvisfb.goldprice;
 
-import android.os.Bundle;
-
-import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
-import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.smixx.fabric.FabricPackage;
-
-import java.util.Arrays;
-import java.util.List;
-
-import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends ReactActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
-    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -29,27 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "GoldPrice";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new FabricPackage(this),
-            new ReactNativeLocalizationPackage()
-        );
     }
 }
